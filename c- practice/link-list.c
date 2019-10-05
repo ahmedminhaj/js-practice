1e124ad0cc5827645884d4d8605ce04edcd3a16c
ahmedminhaj <ahmedminhaj.pro@gmail.com>
2019-10-05 13:29:34 +0600

    link-list CRUD

--- c- practice/link-list.c
+++ c- practice/link-list.c
@@ -7,18 +7,18 @@ struct Node{
 	struct Node* next;
 };
 
-struct Node *createNode(int value){
-	struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+struct Node* createNode(int value){
+	struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
 	temp->data = value;
 	temp->previous = temp->next = NULL;
 	return temp;
 }
 
-void orderList(struct Node *node){
+void printList(struct Node* node){
 	if(node != NULL){
-		orderList(node->previous);
+		printList(node->previous);
 		printf("%d ", node->data);
-		orderList(node->next);
+		printList(node->next);
 	}	
 }
 
@@ -32,19 +32,61 @@ struct Node* insert(struct Node* node, int data){
 	return node;
 }
 
+struct Node* minValueNode(struct Node* node){ 
+    struct Node* current = node; 
+    while (current && current->previous != NULL) 
+        current = current->previous; 
+    return current; 
+}
+
+struct Node* delete(struct Node* node, int data){ 
+    if (node == NULL) return node;  
+    if (data < node->data) 
+        node->previous = delete(node->previous, data); 
+    else if (data > node->data) 
+        node->next = delete(node->next, data); 
+    else{ 
+        if (node->previous == NULL) { 
+            struct Node *temp = node->next; 
+            free(node); 
+            return temp; 
+        }else if (node->next == NULL) { 
+            struct Node *temp = node->previous; 
+            free(node); 
+            return temp; 
+        } 
+        struct Node* temp = minValueNode(node->next); 
+        node->data = temp->data; 
+        node->next = delete(node->next, temp->data); 
+    } 
+    return node; 
+}
+
+struct Node* update(struct Node* node, int data, int newdata) { 
+    node = delete(node, data); 
+    node = insert(node, newdata); 
+    return node; 
+}  
+
 int main(){
 	struct Node *head = NULL;
 	head = insert(head, 9);
 	insert(head, 7);
 	insert(head, 11);
-	orderList(head);
-	printf("\n");
-	
+	printList(head);
+	printf("\n");	
 	insert(head, 15);
 	insert(head, 5);
 	insert(head, 2);
 	insert(head, 8);
-	orderList(head);
+	printList(head);
+	printf("\n");
+	head = delete(head, 7);
+	printList(head);
+	printf("\n");
+	head = update(head, 8, 6);
+	head = update(head, 11, 16);
+	printList(head);
 	printf("\n");
 	return 0;
 }
